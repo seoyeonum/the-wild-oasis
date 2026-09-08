1e7b5f1e09fb885dfbbaea363a7cd4aca2d57f28
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-09-08 22:28:11 +0900

    ✨ feat: detect the click outside the Modal
    - use useOutsideClick to detect clicks outside the Modal
    - attach the returned ref to StyledModal
    - close the Modal when clicking outside its content

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,7 +1,6 @@
 import Button from '../../ui/Button';
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal';
-import CabinTable from './CabinTable';
 
 function AddCabin() {
   return (
@@ -14,38 +13,8 @@ function AddCabin() {
       <Modal.Window name="cabin-form">
         <CreateCabinForm />
       </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button variation="primary" size="medium">
-          Show table
-        </Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
     </Modal>
   );
 }
-
-// function AddCabin() {
-//   const [isOpenModal, setisOpenModal] = useState(false);
-
-//   return (
-//     <div>
-//       <Button
-//         variation="primary"
-//         size="medium"
-//         onClick={() => setisOpenModal((show) => !show)}
-//       >
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setisOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setisOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 export default AddCabin;
