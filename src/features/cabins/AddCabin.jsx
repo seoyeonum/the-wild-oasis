685e9c7760b60d2ae6368016806cc621997827fd
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-09-09 23:52:20 +0900

    ✨ feat: connect edit and delete actions to Modal
    - open the Modal when clicking the Edit or Delete button
    - connect the Edit Modal to the update functionality
    - connect the Delete Modal to the delete functionality

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,16 +4,18 @@ import Modal from '../../ui/Modal';
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button variation="primary" size="medium">
-          Add new cabin
-        </Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button variation="primary" size="medium">
+            Add new cabin
+          </Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
