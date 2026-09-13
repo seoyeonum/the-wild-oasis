13872c1e36b66779c460a1566923703cae74c2ff
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-09-13 16:03:03 +0900

    ✨ feat: implement client-side sorting for cabins
    - store sort field and options in URL search params
    - implement client-side sorting for cabins
    - make the SortBy component reusable

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -9,11 +9,12 @@ import { useSearchParams } from 'react-router-dom';
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
-  const [serachParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
 
-  const filterValue = serachParams.get('discount') || 'all';
+  // 1) FILTER
+  const filterValue = searchParams.get('discount') || 'all';
 
   let filteredCabins;
   if (filterValue === 'all') filteredCabins = cabins;
@@ -21,6 +22,14 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2) SORT
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier,
+  );
 
   return (
     <Menus>
@@ -35,7 +44,8 @@ function CabinTable() {
         </Table.Header>
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
